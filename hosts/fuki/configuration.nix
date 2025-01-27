56f8711043eea91cf7af2359c18c50d769c00c59
winterunderscore <66862891+winterunderscore@users.noreply.github.com>
2025-01-27 18:59:13 +0800

    fix audio

--- hosts/fuki/configuration.nix
+++ hosts/fuki/configuration.nix
@@ -62,11 +62,10 @@
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
+    jack.enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
   };
 
   # Install firefox.
